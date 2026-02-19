c2543122693551d225fbe3048c5831f684117e7f
Michael Haaf <michael.haaf@gmail.com>
2026-02-19 18:00:36 -0500

    font config

--- home/michael/common/core/fonts.nix
+++ home/michael/common/core/fonts.nix
@@ -1,7 +1,20 @@
 { pkgs, ... }:
 {
-  fonts.fontconfig.enable = true;
   home.packages = [
     pkgs.noto-fonts
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.iosevka
+    pkgs.nerd-fonts.ubuntu-sans
+    pkgs.nerd-fonts.ubuntu
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "pkgs.nerd-fonts.iosevka" ];
+      sansSerif = [ "pkgs.nerd-fonts.ubuntu-sans" ];
+      serif = [ "pkgs.nerd-fonts.ubuntu" ];
+
+    };
+  };
 }
