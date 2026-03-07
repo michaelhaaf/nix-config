5ef1d24dbdd7f5ec121ac83a67d618ab7518fe9e
michaelhaaf <michael.haaf@gmail.com>
2026-03-06 19:03:43 -0500

    a fix

--- hosts/common/core/ssh.nix
+++ hosts/common/core/ssh.nix
@@ -4,6 +4,8 @@
   ...
 }:
 {
+  # TODO: Some submodule seems to be enabling this?
+  services.gnome.gcr-ssh-agent.enable = false;
   programs.ssh = lib.optionalAttrs pkgs.stdenv.isLinux {
     startAgent = true;
     # enableAskPassword = true;
