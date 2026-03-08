2d4d785b69cbb4a54cfecde1e6c24b2d94cd7eca
michaelhaaf <michael.haaf@gmail.com>
2026-03-08 14:53:40 -0400

    greetd warning fix

--- hosts/common/optional/services/greetd.nix
+++ hosts/common/optional/services/greetd.nix
@@ -33,7 +33,7 @@ in
       restart = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --asterisks --time --time-format '%I:%M %p | %a • %h | %F' --cmd niri";
+          command = "${pkgs.tuigreet}/bin/tuigreet --asterisks --time --time-format '%I:%M %p | %a • %h | %F' --cmd niri";
           user = "michael";
         };
 
