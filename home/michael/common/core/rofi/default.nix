55418de9aa6e4636f096825be434eae9aea9f375
Michael Haaf <michael.haaf@gmail.com>
2026-03-19 10:37:43 -0400

    attempt to control the rofi theme, no luck so far

--- home/michael/common/core/rofi/default.nix
+++ home/michael/common/core/rofi/default.nix
@@ -1,12 +1,14 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
-  stylix.targets.rofi.enable = true;
   xdg.configFile."rofi-pass/rofi-pass.rasi".source = ./rofi-pass.rasi;
   programs.rofi = {
     enable = true;
+    theme = lib.mkDefault "~/.nix-profile/share/rofi/themes/Arc-Dark";
+    font = lib.mkDefault "IosevkaTermSlab NF 16";
     pass = {
       enable = true;
       package = pkgs.rofi-pass-wayland;
