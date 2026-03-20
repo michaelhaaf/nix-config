9c8cbd425241475b3149fb05c3a98667bcbaab4e
Michael Haaf <michael.haaf@gmail.com>
2026-03-19 23:37:24 -0400

    attempt to add otpauth qr code processing to pass

--- home/michael/common/core/password-store.nix
+++ home/michael/common/core/password-store.nix
@@ -1,8 +1,31 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
+  home.packages =
+    let
+      qrencode = lib.getExe pkgs.qrencode;
+      wlCopy = lib.getExe' pkgs.wl-clipboard "wl-copy";
+      wlPaste = lib.getExe' pkgs.wl-clipboard "wl-paste";
+      zbarimg = lib.getExe' pkgs.zbar "zbarimg";
+    in
+    [
+      pkgs.zbar
+      pkgs.qrencode
+      # Adapted from https://github.com/hyperparabolic/nix-config
+      # helpers for encoding and decoding otp qrs from clipboard for backup
+      # TODO: this doesn't really work
+      (pkgs.writeShellScriptBin "otpauth-decode-clipboard" ''
+        ${wlPaste} | ${zbarimg} -q --raw -
+        ${wlCopy} --clear
+      '')
+      (pkgs.writeShellScriptBin "otpauth-encode-clipboard" ''
+        ${wlPaste} | ${qrencode} -m 2 -t utf8
+        ${wlCopy} --clear
+      '')
+    ];
   # TODO: pull password store from remote git repo
   programs.password-store = {
     enable = true;
