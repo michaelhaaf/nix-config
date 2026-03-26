3d803317c37363e3818cb988db684d7818e2c586
Michael Haaf <michael.haaf@gmail.com>
2026-03-26 12:05:30 -0400

    improve direnv

--- home/michael/common/core/direnv.nix
+++ home/michael/common/core/direnv.nix
@@ -1,8 +1,18 @@
 {
+  pkgs,
+  ...
+}:
+{
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
     nix-direnv.enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
+    nix-direnv.package = pkgs.unstable.nix-direnv;
+    config = {
+      global = {
+        log_filter = "^loading"; # https://github.com/direnv/direnv/issues/203#issuecomment-3061299852
+      };
+    };
   };
 }
