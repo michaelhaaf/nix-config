5f8735a2e9c473c9c523ae63c03c515e0ceefd26
Michael Haaf <michael.haaf@gmail.com>
2026-08-14 19:26:40 -0400

    niri-flake fix

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     niri-flake = {
-      url = "github:sodiboo/niri-flake";
+      url = "github:epireyn/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
